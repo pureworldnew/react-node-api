f38e1658839cefc8575a370c6eff4193f5921827
pureworldnew <pureworldnew@gmail.com>
2021-09-27 00:24:56 -0400

    update fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 //serve the react app files
-app.use(express.static(`${__dirname}/ui-react/build`));
+// app.use(express.static(`${__dirname}/ui-react/build`));
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello World!" });
